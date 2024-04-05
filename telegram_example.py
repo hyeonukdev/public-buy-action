259cdf90f5749ceef607ef90221c9ef2aa46564b
hyeonukdev <lego7407@gmail.com>
2024-04-06 01:30:36 +0900

    Fix: telegram lib telebot

--- telegram_example.py
+++ telegram_example.py
@@ -1,10 +1,32 @@
-import telegram
-import asyncio
+# from telegram import Bot
+
+# import asyncio
 
 
-BOT_TOKEN = ''
-CHAT_ID = ''
-text = 'Bot에서 보낸 메세지입니다.'
+# BOT_TOKEN = '7042471860:AAEGGb2usb6ZjZomisNmyEEB1SytBqSXrsM'
+# CHAT_ID = '770967266'
+# text = 'text'
 
-bot = telegram.Bot(token = BOT_TOKEN)
-asyncio.run(bot.sendMessage(chat_id=CHAT_ID, text=text))
+# def send_message(text):
+#     """
+#     동기적으로 Telegram 메세지를 보내는 함수
+#     :param text: 보낼 메세지의 내용
+#     """
+#     bot = Bot(token=BOT_TOKEN)
+#     bot.send_message(chat_id=CHAT_ID, text=text)
+
+# # 함수 사용 예제
+# send_message(text)
+
+
+import telebot
+
+BOT_TOKEN = '7042471860:AAEGGb2usb6ZjZomisNmyEEB1SytBqSXrsM'
+CHAT_ID = '770967266'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+def send_message(text):
+    bot.send_message(CHAT_ID, text)
+
+send_message('Bot에서 보낸 메세지입니다.')
